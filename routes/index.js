f32fe0e4f0d3091cf99f0bb23ad18aeb7aaf8e4d
Julian Korgol <kontakt@juliankorgol.com>
2022-05-11 22:08:21 +0200

    IF admin

--- routes/index.js
+++ routes/index.js
@@ -1,3 +1,5 @@
+// if (req.session.userLogin === "admin")
+
 const express = require('express')
 const req = require('express/lib/request')
 const sql = require('mssql')
@@ -78,23 +80,26 @@ async function showLoginForm(req, res) {
 
 async function login(req, res) {
   const {login, password} = req.body;
+  if (req.session.userLogin === "admin") {
+    try {
+      const dbRequest = await request()
 
-  try {
-    const dbRequest = await request()
+      const result = await dbRequest
+          .input('Login', sql.VarChar(50), login)
+          .input('Haslo', sql.VarChar(50), password)
+          .query('SELECT Login FROM Uzytkownicy WHERE Login = @Login AND Haslo = @Haslo')
 
-    const result = await dbRequest
-      .input('Login', sql.VarChar(50), login)
-      .input('Haslo', sql.VarChar(50), password)
-      .query('SELECT Login FROM Uzytkownicy WHERE Login = @Login AND Haslo = @Haslo')
-  
-    if (result.rowsAffected[0] === 1) {
-      req.session.userLogin = login;
-      showProducts(req, res);
-    } else {
+      if (result.rowsAffected[0] === 1) {
+        req.session.userLogin = login;
+        showProducts(req, res);
+      } else {
+        res.render('login', {title: 'Logownie', error: 'Logowanie nieudane'})
+      }
+    } catch (err) {
       res.render('login', {title: 'Logownie', error: 'Logowanie nieudane'})
     }
-  } catch (err) {
-    res.render('login', {title: 'Logownie', error: 'Logowanie nieudane'})
+  } else {
+    res.render('login', {title: 'Logownie', error: 'Logowanie zablokowane'})
   }
 
 }
